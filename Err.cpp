d4d048bf0f5d66c800691afb28bc5e54a2e87a24
wenjiehs <wenjiehs@outlook.com>
2015-05-12 20:00:41 -0700

    1.add new file

---
+++ Err.cpp
@@ -0,0 +1,81 @@
+#include "../include/Err.h"
+
+Err::Err()
+{
+    //ctor
+}
+
+Err::~Err()
+{
+    //dtor
+}
+void Err::err_doit(int errnoFlag, int error, const char * fmt, va_list ap)
+{
+    char Buff[MAXLINE];
+    vsnprintf(Buff, MAXLINE - 1, fmt, ap);
+    if(errnoFlag)
+    {
+        snprintf(Buff + strlen(Buff), MAXLINE - strlen(Buff) - 1, ": %s", strerror(error));
+        strcat(Buff, "\n");
+        fflush(stdout);
+        fputs(Buff, stderr);
+        fflush(NULL);
+    }
+}
+void Err::ret(const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    err_doit(1, errno, fmt, ap);
+    va_end(ap);
+}
+void Err::sys(const char * fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    err_doit(1, errno, fmt, ap);
+    va_end(ap);
+    exit(1);
+}
+
+void Err::cont(int error, const char * fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    err_doit(1, error, fmt, ap);
+    va_end(ap);
+}
+void Err::Exit(int error,  const char * fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    err_doit(1, error, fmt, ap);
+    va_end(ap);
+    exit(1);
+}
+
+void Err::quit(const char * fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    err_doit(0,0, fmt, ap);
+    va_end(ap);
+}
+
+void Err::dump(const char * fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    err_doit(1, errno, fmt, ap);
+    va_end(ap);
+    abort();
+    exit(1);
+}
+
+void Err::msg(const char * fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    err_doit(0,0, fmt, ap);
+    va_end(ap);
+}
---
+++ Err.h
@@ -0,0 +1,28 @@
+#ifndef ERR_H
+#define ERR_H
+#include <errno.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+class Err
+{
+    public:
+        Err();
+        virtual ~Err();
+
+        static void ret(const char * fmt, ...);
+        static void sys(const char * fmt, ...);
+        static void dump(const char * fmt, ...);
+        static void Exit(int error, const char * fmt, ...);
+        static void cont(int error, const char * fmt, ...);
+        static void quit(const char * fmt, ...);
+        static void msg(const char * fmt, ...);
+    protected:
+        static void err_doit(int, int, const char *, va_list);
+    private:
+        static const int MAXLINE = 4096;
+};
+
+#endif // ERR_H
